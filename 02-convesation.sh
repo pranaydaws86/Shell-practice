dfec043849aea89dc85f45285e07378058805fac
pranay <pranaydaws86@gmail.com>
2025-11-20 23:31:51 +0530

    Variables

--- 02-convesation.sh
+++ 02-convesation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "Pranay:: How are you shravani ?
+echo
+     "Pranay:: How are you shravani ?"
      "Shravani:: I am good pranay, how about you ?
      "Pranay:: am good too"
      "Shravani:: nice to hear that"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Pedda Aggu
+PERSON2=Chinna Aggu
+
+echo "PERSON1":: Hi "PERSON2",how are u?"
+echo "PERSON2":: i am good "PERSON1", how about you"
+echo "PERSON1":: what you are doing?"
+echo "PERSON2":: Nothing practicing shell-scripting"
