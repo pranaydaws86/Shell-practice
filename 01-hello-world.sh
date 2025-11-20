c59f8939712de9ce7f020a6a7cf29f19e45de5f3
pranay <pranaydaws86@gmail.com>
2025-11-20 22:50:22 +0530

    conversation

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+l#!/bin/bash
 
 "Welcome to Shell-scripting pranay"
 "Finally I suceeded in solving the mystery which am trying to chase over the past one hour"
---
+++ convesation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Pranay:: How are you shravani ?
+     "Shravani:: I am good pranay, how about you ?
+     "Pranay:: am good too"
+     "Shravani:: nice to hear that"
