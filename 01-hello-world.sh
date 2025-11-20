b72f65d9211531649d6c5df31f3050192adec42f
pranay <pranaydaws86@gmail.com>
2025-11-20 22:08:38 +0530

    this is my 3rd commit

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,3 +2,4 @@
 
 "Welcome to Shell-scripting pranay"
 "Finally I suceeded in solving the mystery which am trying to chase over the past one hour"
+"Today i fee bad about my practice , i questioned my self am i putting enough efforts in achieving my goal"
