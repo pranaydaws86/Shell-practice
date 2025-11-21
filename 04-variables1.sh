8c82078800c6cb7ab67b795e7384eab03713877c
pranay <pranaydaws86@gmail.com>
2025-11-21 21:36:51 +0530

    Variables

--- 04-variables1.sh
+++ 04-variables1.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1::hi how are you $PERSON2?"
-echo "$PERSON2::am good $PERSON1, how about you?"
-echo "$PERSON1::am good too $PERSON2, what's up?"
-echo "$PERSON2::Nothing u have to say"
+echo "$PERSON1:: hi how are you $PERSON2?"
+echo "$PERSON2:: am good $PERSON1, how about you?"
+echo "$PERSON1:: am good too $PERSON2, what's up?"
+echo "$PERSON2:: Nothing u have to say"
