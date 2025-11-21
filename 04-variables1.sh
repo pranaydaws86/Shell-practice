8ebdfd23dd73e0dfe73c8fddcaddd6584682375a
pranay <pranaydaws86@gmail.com>
2025-11-21 21:25:37 +0530

    variables1

---
+++ 04-variables1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "PERSON1::hi how are you ?"
+echo "PERSON2::am good, how about you?"
+echo "PERSON1::am good too, what's up?"
+echo "PERSON2::Nothing u have to say"
